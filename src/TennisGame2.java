5074f15c8efa6f374abd8955c0b14dd23d4150d1
Allen Vargas <allenbfox@gmail.com>
2020-02-20 01:43:36 -0400

    Aplicar getLiteral en partes del codigo que retornen el literal de algun numero
    Logramos la reeutilizacion de coidgo y disminuimos codigo repetido

--- src/TennisGame2.java
+++ src/TennisGame2.java
@@ -33,14 +33,8 @@ public class TennisGame2 implements TennisGame
 	private String normal3(String score) {
 		if (P2point>P1point && P2point < 4)
         {
-            if (P2point==2)
-                P2res="Thirty";
-            if (P2point==3)
-                P2res="Forty";
-            if (P1point==1)
-                P1res="Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
+			P1res = getLiteral(P1point);
+            P2res = getLiteral(P2point);
             score = P1res + "-" + P2res;
         }
 		return score;
@@ -49,14 +43,8 @@ public class TennisGame2 implements TennisGame
 	private String normal2(String score) {
 		if (P1point>P2point && P1point < 4)
         {
-            if (P1point==2)
-                P1res="Thirty";
-            if (P1point==3)
-                P1res="Forty";
-            if (P2point==1)
-                P2res="Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
+			P1res = getLiteral(P1point);
+            P2res = getLiteral(P2point);
             score = P1res + "-" + P2res;
         }
 		return score;
@@ -65,14 +53,8 @@ public class TennisGame2 implements TennisGame
 	private String normal1(String score) {
 		if (P2point > 0 && P1point==0)
         {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
-            
-            P1res = "Love";
+			P1res = getLiteral(P1point);
+            P2res = getLiteral(P2point);
             score = P1res + "-" + P2res;
         }
 		return score;
@@ -82,7 +64,7 @@ public class TennisGame2 implements TennisGame
 		if (P1point > 0 && P2point==0)
         {
             P1res = getLiteral(P1point);
-            P2res = "Love";
+            P2res = getLiteral(P2point);
             score = P1res + "-" + P2res;
         }
 		return score;
@@ -129,12 +111,7 @@ public class TennisGame2 implements TennisGame
 	private String tie(String score) {
 		if (P1point == P2point && P1point < 4)
         {
-            if (P1point==0)
-                score = "Love";
-            if (P1point==1)
-                score = "Fifteen";
-            if (P1point==2)
-                score = "Thirty";
+            score = getLiteral(P1point);
             score += "-All";
         }
 		return score;
